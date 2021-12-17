fd33281829549f5e253b365e78977a188ea2442f
lycheeez1 <53366206+lycheeez1@users.noreply.github.com>
2021-12-18 02:30:05 +0900

    Update register-check.php

--- menu/notice/register-check.php
+++ menu/notice/register-check.php
@@ -10,6 +10,16 @@
 
   include ("../../authentication/dbconn.php");
 
+  if ($_FILES['attached_file']['tmp_name']) {
+    $tmp_file =  $_FILES['attached_file']['tmp_name'];
+    $ori_name = $_FILES['attached_file']['name'];
+    $filename = iconv("UTF-8", "EUC-KR", $_FILES['attached_file']['name']);
+    $file_dir = "../../upload/".$filename;
+    move_uploaded_file($tmp_file, $file_dir);
+  } else {
+    $ori_name = NULL;
+  }
+
   $dup_check = "SELECT * FROM notice WHERE NTITLE='$title' and NBODY='$body'";
   $chk_res = mysqli_query($conn, $dup_check);
   $rowNum = mysqli_num_rows($chk_res);
@@ -20,12 +30,18 @@
           </script>");
   }
   else {
-    $sql = "INSERT INTO notice(NTITLE, NBODY, NDATE, NHIT) VALUES('$title', '$body', '$date', 0)";
+    $sql = "INSERT INTO notice(NTITLE, NBODY, NDATE, NHIT, NFILE) VALUES('$title', '$body', '$date', 0, '$ori_name')";
     $res = mysqli_query($conn, $sql);
+    echo("<script>alert('hello2');</script>");
     if ($res) {
       echo("<script>
               alert('공지가 정상적으로 등록되었습니다.');
               location.href='../noticeList.php';
+            </script>");
+    } else {
+      echo("<script>
+              alert('비정상적인 접근입니다.');
+              location.href='./register.php';
             </script>");
     }
   }
