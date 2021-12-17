c970e0174cfed9bf42a5b4004e139d4235d1db2e
lycheeez1 <53366206+lycheeez1@users.noreply.github.com>
2021-12-18 02:28:03 +0900

    Update modify.php

--- menu/notice/modify.php
+++ menu/notice/modify.php
@@ -22,98 +22,111 @@
   $body = $notice['NBODY'];
 ?>
 
+<?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 0);
+  session_start();
+
+  if(!isset($_SESSION['ADMIN'])) {
+    echo("<script>
+            alert('글 작성 권한이 없습니다.');
+            location.href='../noticeList.php';
+          </script>");
+  }
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
-  <title>관리자 페이지 - 공지사항 등록</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>공지사항 등록</title>
+  <!-- include libraries(jQuery, bootstrap) -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <!-- include summernote css/js -->
+  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#summernote').summernote({
+        lang: 'ko-KR',
+        height: 300,
+        minHeight: null,
+        maxHeight:null,
+        toolbar: [
+          // [groupName, [list of button]]
+          ['style', ['bold', 'italic', 'underline', 'clear']],
+          ['font', ['strikethrough', 'superscript', 'subscript']],
+          ['fontsize', ['fontsize']],
+          ['color', ['color']],
+          ['para', ['ul', 'ol', 'paragraph']],
+          ['height', ['height']],
+          ['insert', ['picture', 'link', 'video']]
+        ]
+      });
+
+      $(function(){
+        $("#register-btn").click(function(){
+          var summernoteContent = $('#summernote').summernote('code');
+          alert(summernoteContent);
+        });
+      });
+
+      $("#btn-tolist").click(function(){
+        location.href='../noticeList.php';
+      });
+    });
+
+
+  </script>
   <style type="text/css">
+    @font-face {
+        font-family: 'NanumSquareRound';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
     body {
       text-align: center;
-      margin: 30px;
-      font-family: "나눔스퀘어_ac", "나눔고딕";
+      font-family: 'NanumSquareRound';
     }
-    form {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-      width: 80%;
-      height : 80%;
-      max-width: 850px;
-      max-height: 550px;
-      background-color: lightgreen;
+    .form-area {
+      margin: 5%;
     }
-    .toolbar {
-      padding: 10px;
-    }
-    .form-table {
-      text-align: center;
-      margin: 0 auto;
-      margin-top: 10px;
-      background-color: green;
-      width: 90%;
-      padding:10px;
-    }
-    table td {
-      border-bottom: 2px solid #ccc;
-      padding: 5px 0;
-    }
-    table input {
-      display: inline-block;
-      width: 100%;
-      padding: 5px 10px;
-      margin: 5px 0;
-      font-family: "나눔스퀘어_ac", "나눔고딕";
-    }
-    textarea {
-      width: 100%;
-      height: 350px;
-      padding: 10px;
-      font-family: "나눔스퀘어_ac", "나눔고딕";
-    }
-    button {
+    #register-btn {
       width: 100px;
       margin-top: 10px;
       padding: 10px;
-      border: none;
-      border-radius: 2px;
-      color: white;
-      background-color: green;
-      cursor: pointer;
-      font-family: "나눔스퀘어_ac","나눔바른고딕";
       font-size: 12pt;
     }
-    button:hover {
-      background-color: lightgreen;
+    #register-btn:hover {
+      background-color: #7fd87d;
+      color: white;
     }
-    footer {
-      font-size: 10px;
-      font-family: "나눔바른고딕";
+    input[type=file]:hover {
+      cursor:pointer
     }
-    </style>
+  </style>
 </head>
 <body>
-  <h2>공지사항 등록</h2>
-
-  <form method="POST" action="modify-check.php">
-    <div class="toolbar">
-      툴바 영역
-    </div>
-    <table class="form-table">
-      <tr>
-        <td>제목</td>
-        <td><input type="text" name="title" value="<?=$title;?>" required></td>
-      </tr>
-      <tr>
-        <td>내용</td>
-        <td><textarea name="body" required><?=$body;?></textarea></td>
-    </table>
-    <input type="hidden" name="idx" value="<?=$index;?>">
-    <button type="submit">등록</button> <br/>
-    <!--
-    <td><button type="button" href="../../menu/notice.php">취소</button></td>
-    [취소] 눌렀을 때 뒤로 돌아갈지 팝업창 띄우기 -->
-  </form>
-
-
+  <h3>공지사항 등록</h3>
+  <button class="btn contact-btn" id="btn-tolist" style="float:left;margin-left:10%">&lt; 목록으로</button>
+  <div class="form-area">
+    <form method="POST" enctype="multipart/form-data" action="register-check.php">
+      <input type="hidden" name="imgUrl" id="imgUrl" value="">
+      <input type="hidden" name="attachFile" id="attachFile" value="">
+      <div class="form-area">
+        <div>
+          <input type="text" class="form-control" name="title" value="<?=$title;?>" required>
+          <textarea id="summernote" name="body" required><?=$body;?></textarea>
+          <input type="file" value="1" name="attached_file" />
+        </div>
+      </div>
+      <input type="hidden" name="idx" value="<?=$index;?>">
+      <button type="submit" class="btn contact-btn" id="register-btn">등록</button>
+    </form>
+  </div>
 </body>
 </html>
