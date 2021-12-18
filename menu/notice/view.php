1ccd327f9d80d0f3b87a2932b86bcb0bcddc10e6
lycheeez1 <53366206+lycheeez1@users.noreply.github.com>
2021-12-18 23:16:00 +0900

    Add files via upload

--- menu/notice/view.php
+++ menu/notice/view.php
@@ -186,8 +186,8 @@
     }
   ?>
   <div class="user-cont">
-    <div class="btn-login"><a href="../authentication/login.html">로그인</a></div>
-    <div class="btn-logout"><a href="../authentication/logout.php">로그아웃</a></div>
+    <div class="btn-login"><a href="../../authentication/login.html">로그인</a></div>
+    <div class="btn-logout"><a href="../../authentication/logout.php">로그아웃</a></div>
     <div class="greeting"><?=$admin;?> 계정 접속 중</div>
   </div>
   <!-- 최상단 로그인 영역 end -->
